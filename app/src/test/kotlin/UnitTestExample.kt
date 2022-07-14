10a85635107a579b8923df946ba54a83078961b2
Helena Yang <hfy@fb.com>
2022-07-13 17:03:31 -0700

    init unit test

---
+++ app/src/test/kotlin/UnitTestExample.kt
@@ -0,0 +1,11 @@
+package com.dev.moosic
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class UnitTestExample {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, (2 + 2).toLong())
+    }
+}
